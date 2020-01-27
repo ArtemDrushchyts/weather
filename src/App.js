71cf06137d9b2b378816ec5acf6b3e80695a2a6c
Artem Drushchyts <temafm-3@yandex.ru>
2020-01-28 00:02:00 +0300

    feat: added data to the page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
   state = {
     temp: undefined,
     city: undefined,
+    country: undefined,
+    pressure: undefined,
     sunrise: undefined,
     sunset: undefined,
     error: undefined
@@ -18,13 +20,32 @@ class App extends React.Component {
   gettingWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await api_url.json();
-    console.log(data);
+    
 
-    this.setState({
-      
-    })
+    if(city) {
+      const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await api_url.json();
+      console.log(data);
+
+      let sunset = data.sys.sunset;
+      let date = new Date();
+      date.setTime(sunset);
+      let sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      let sunrise = data.sys.sunrise;
+      date.setTime(sunrise);
+      let sunrise_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunrise: sunrise_date,
+        sunset: sunset_date,
+        error: ''
+      })
+    }
   }
 
   render() {
@@ -32,7 +53,15 @@ class App extends React.Component {
       <div>
         <Info/>
         <Form weatherMethod={this.gettingWeather} />
-        <Weather/>
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          pressure={this.state.pressure}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div> 
     )
   }
